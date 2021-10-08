e621b7388edd1f3d8b1928036550f2db0e80c04b
leandro16197 <49287681+leandro16197@users.noreply.github.com>
2021-10-08 13:09:35 -0300

    Update Busqueda.java

--- src/Busqueda.java
+++ src/Busqueda.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Comparator;
-import Tpe_Programacion.Partiipante;
 public interface  Busqueda {
     ArrayList compare(ArrayList lista1,Participante p);
 }
